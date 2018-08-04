5d27e37006e350c7b5e5a8b7061cf96436b6b806
WELCOME <WELCOME@WELCOME-PC>
2018-08-04 18:34:37 +0530

    Srikanth Again

---
+++ src/main/java/week1/HomeWorkDay1/ArithmeticOperations.java
@@ -0,0 +1,41 @@
+package week1.HomeWorkDay1;
+
+class ArithmeticOperations 
+{
+	static int numberOne=22;
+	static int numberTwo=44;
+
+	public void addition() 
+	{
+		System.out.println("Sum of Two Number : ");
+		System.out.println(numberOne  + numberTwo);
+	}  
+	
+	public void multiply() 
+	{
+		System.out.println("Product of Two Number : ");
+		System.out.println(numberOne  * numberTwo);
+	}  
+	
+	public void subtract() 
+	{
+		System.out.println("Diference of Two Number : ");
+		System.out.println(numberTwo  - numberOne);
+	}  
+	
+	public void multiplyNoByTwo() 
+	{
+		System.out.println("First number multiplied by 2 :");
+		System.out.println(numberOne  * 2);
+		System.out.println("Second number multiplied by 2 :");
+		System.out.println(numberTwo  * 2);
+	}  
+	
+	public void divideNoByTwo() 
+	{
+		System.out.println("First number divided by 2 :");
+		System.out.println(numberOne  / 2);
+		System.out.println("First number divided by 2 :");
+		System.out.println(numberTwo  / 2);
+	} 
+}
