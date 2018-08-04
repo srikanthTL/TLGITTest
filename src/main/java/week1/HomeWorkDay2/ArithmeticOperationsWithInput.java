f6903f4a288ea081f35cf45160a4a0106c53a2c6
WELCOME <WELCOME@WELCOME-PC>
2018-08-04 18:32:50 +0530

    Srikanth Again - changes done

--- src/main/java/week1/HomeWorkDay2/ArithmeticOperationsWithInput.java
+++ src/main/java/week1/HomeWorkDay2/ArithmeticOperationsWithInput.java
@@ -23,7 +23,7 @@ public class ArithmeticOperationsWithInput
 	public static void subtract(int numberone, int numbertwo) 
 	{
 		//Conditional or Relational operators
-		\\comit
+		//Removed the Comment
 		
 		System.out.println("\n Diference of Two Number : \n");
 		if (numberone >= numbertwo)
